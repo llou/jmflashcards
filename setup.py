2d21e6e0b5410a7212115ed3a2b44d5fa54fb7fa
Jorge Monforte González <yo@llou.net>
2019-11-25 15:51:25 +0100

    set up setup.py to upload it to PyPI

--- setup.py
+++ setup.py
@@ -7,23 +7,28 @@ from setuptools import setup
 PACKAGE_PATH=os.path.dirname(os.path.realpath(__file__))
 VERSION=open(os.path.join(PACKAGE_PATH, 'VERSION')).read().strip()
 
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
 setup(name = 'jmflashcards',
       version = VERSION,
-      description = 'A flashcard manager',
+      description = 'A Flashcard deluxe flashcard processor',
+      long_description = long_description,
       author = 'Jorge Monforte González',
-      author_email = 'jorge.monforte@gmail.com',
+      author_email = 'yo@llou.net',
+      url = 'https://github.com/llou/jmflashcards',
       scripts = ['bin/jmflashcards'],
       packages = ['jmflashcards'],
       package_dir = {'' : 'lib'},
-      license = 'private',
+      license = 'MiT',
       keywords = 'flashcards latex',
       classifiers = [
           'Environment :: Console',
-          'Development Status :: 2 - Pre-Alpha',
+          'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Education',
           'Intended Audience :: End Users/Desktop',
           'Intended Audience :: Developers',
-          'License :: Other/Proprietary License',
+          'License :: OSI Approved :: MIT License',
           'Natural Language :: English',
           'Operating System :: POSIX',
           'Programming Language :: Python :: 2.7',
