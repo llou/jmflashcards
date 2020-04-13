d684f30807e9bf5e7dd0e2d145c2dc3881043372
Jorge Monforte González <yo@llou.net>
2020-04-13 21:18:09 +0200

    fix requirements

--- jmflashcards/logs.py
+++
@@ -1,9 +0,0 @@
-import sys
-from logging import getLogger, StreamHandler, DEBUG, INFO, WARNING, ERROR, CRITICAL
-
-def get_logger(level=WARNING):
-    logger = getLogger()
-    logger.setLevel(level)
-    logger.setHandler(StreamHandler(stream=sys.stderr))
-    return logger
-
